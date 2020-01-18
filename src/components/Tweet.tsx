862593905018b09be2736defd92d5a7b5447ad46
Stijn van der Pol <s.o.vanderpol@gmail.com>
2020-01-18 21:53:45 +0100

    add a tweet component

---
+++ src/components/Tweet.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+export interface Props {
+    username: string;
+    handle: string;
+    profilePictureUrl: string;
+    text: string;
+}
+
+export const Tweet = ({username, handle, profilePictureUrl, text}: Props) => {
+    const classes = createUseStyles({
+        container: {
+            width: 700,
+            border: '1px solid #dedede',
+            display: 'flex',
+            flexDirection: 'column'
+        },
+        userInfoContainer: {
+            display: 'flex',
+            flexDirection: 'row'
+        },
+        profilePicture: {
+            borderRadius: '100%'
+        },
+        nameContainers: {
+            display: 'flex',
+            flexDirection: 'column'
+        },
+        username: {
+            fontWeight: 'bold'
+        },
+        handle: {
+            color: '#dedede'
+        }
+    })();
+
+
+    return (
+        <div className={classes.container}>
+            <div className={classes.userInfoContainer}>
+                {profilePictureUrl && <img className={classes.profilePicture} src={profilePictureUrl} />}
+                <div className={classes.nameContainers}>
+                <span className={classes.username}>{username}</span>
+                <span className={classes.handle}>{handle}</span>
+                </div>
+            </div>
+            <p>
+                {text}
+            </p>
+        </div>
+    );
+}
