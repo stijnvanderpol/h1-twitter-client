6c8e88c495d48d85470a29a68d97278ad5adfb39
Stijn van der Pol <s.o.vanderpol@gmail.com>
2020-01-18 23:00:27 +0100

    wrap the app in a store provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 import {HomePage} from './pages/HomePage';
 
-ReactDOM.render(<HomePage />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+    <HomePage />
+</Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
