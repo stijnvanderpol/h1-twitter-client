d59581b4bb8116de462ba4e9045f8e0c00e85b95
Stijn van der Pol <s.o.vanderpol@gmail.com>
2020-01-18 23:52:51 +0100

    add the connected top 10 hashtags component to the homepage and clean up the styling

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,20 +3,35 @@ import {createUseStyles} from 'react-jss'
 
 import { TweetSearchField } from '../containers/TweetSearchField';
 import { ConnectedTweetList } from '../containers/ConnectedTweetList';
+import { ConnectedTop10Hashtags } from '../containers/ConnectedTop10Hashtags';
 
 export const HomePage = () => {
     const classes = createUseStyles({
         'container': {
+            display: 'flex',
+            flexDirection: 'row',
             width: 1200,
             marginLeft: 'auto',
             marginRight: 'auto'
+        },
+        main: {
+            width: 800
+        },
+        sidebar: {
+            width: 400
         }
     })();
 
     return (
         <div className={classes.container}>
-            <TweetSearchField />
-            <ConnectedTweetList />
+            <div className={classes.main}>
+                <TweetSearchField />
+                <ConnectedTweetList />
+            </div>
+            <div className={classes.sidebar}>
+                <h1>Top 10 hashtags</h1>
+                <ConnectedTop10Hashtags />
+            </div>
         </div>
     )
 };
