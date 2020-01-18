36a53a6c0d1b4c6c248784d90b774773984b1a34
Stijn van der Pol <s.o.vanderpol@gmail.com>
2020-01-18 22:12:21 +0100

    refactor SearchField to be more reusable

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,20 +1,29 @@
 import React, { useState, ChangeEvent, useEffect } from 'react';
 import { useDebounce } from 'use-debounce';
 
-export const SearchField = () => {
+export interface Props {
+    onSearchHandler: (query: string) => void;
+    debounce: number;
+    placeholder: string;
+}
+
+export const SearchField = ({onSearchHandler, debounce = 500, placeholder}: Props) => {
 
     const [searchString, setSearchString] = useState('')
-    const [debouncedSearchString] = useDebounce(searchString, 500);
+    const [debouncedSearchString] = useDebounce(searchString, debounce);
 
     const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
         setSearchString(event.target.value);
     }
 
     useEffect(() => {
-        // search the API
+        if (debouncedSearchString.trim().length) {
+            onSearchHandler(debouncedSearchString)
+        }
+
     }, [debouncedSearchString]);
 
     return (
-        <input type="text" onChange={onChangeHandler} />
+        <input type="text" onChange={onChangeHandler} placeholder={placeholder} />
     );
 }
