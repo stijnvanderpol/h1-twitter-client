b98a3d27873a947d0d0a3cd12be44dab46a9af46
Stijn van der Pol <s.o.vanderpol@gmail.com>
2020-01-18 23:22:49 +0100

    replace the typescript type literal with the HashtagCount type

--- src/redux/actions/tweets.ts
+++ src/redux/actions/tweets.ts
@@ -1,12 +1,12 @@
 import * as types from '../types/tweets';
-import { Tweet } from '../../types';
+import { Tweet, HashtagCount } from '../../types';
 
 export const setTweets = (tweets: Tweet[]) => ({
     type: types.SET_TWEETS,
     payload: tweets  
 });
 
-export const setTop10Hashtags = (top10Hashtags: string) => ({
+export const setTop10Hashtags = (top10Hashtags: HashtagCount) => ({
     type: types.SET_TOP_10_HASHTAGS,
     payload: top10Hashtags
 });
