84620e63da867872ae77af9f0ba2d62bd2c1575a
Stijn van der Pol <s.o.vanderpol@gmail.com>
2020-01-18 23:23:00 +0100

    update the store after fetching the tweets

--- src/containers/TweetSearchField.tsx
+++ src/containers/TweetSearchField.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { SearchTweets, Tweet, HashtagCount } from '../types';
+
+import { setTweets, setTop10Hashtags } from '../redux/actions/tweets';
+
 import { SearchField } from '../components/SearchField';
 import { get } from '../utils/api';
 
-const onSearchTwitter = (searchQuery: string) => {
-    get('/tweets', searchQuery).then(response => response.json()).then(body => {
-        console.log('body', body);
-    });
+const mapDispatchToProps = {
+    setTweets,
+    setTop10Hashtags
 }
 
-export const TweetSearchField = () => <SearchField onSearchHandler={onSearchTwitter} placeholder="Search for a tweet" />;
+interface Props {
+    setTweets: (tweets: Tweet[]) => void,
+    setTop10Hashtags: (payload: HashtagCount) => void
+}
+
+
+export const TweetSearchField = connect(null, mapDispatchToProps)(
+    ({setTweets, setTop10Hashtags}: Props) => {
+    const onSearchTwitter = (searchQuery: string) => {
+        get('/tweets', searchQuery).then(response => response.json()).then((body: SearchTweets) => {
+            setTweets(body.tweets);
+            setTop10Hashtags(body.meta.top10Hashtags);
+        });
+    }
+    
+    return <SearchField onSearchHandler={onSearchTwitter} placeholder="Search for a tweet" />
+});
+
+
+
