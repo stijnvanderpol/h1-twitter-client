03fb546b4b0fbfd454c923a4bc8852443ea25b16
Stijn van der Pol <s.o.vanderpol@gmail.com>
2020-01-18 22:39:22 +0100

    call the /api/search endpoint when searching with the TwitterSearchField component

--- src/containers/TweetSearchField.tsx
+++ src/containers/TweetSearchField.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { SearchField } from '../components/SearchField';
+import { get } from '../utils/api';
 
 const onSearchTwitter = (searchQuery: string) => {
-    console.log('going to search twitter for', searchQuery);
+    get('/tweets', searchQuery).then(response => response.json()).then(body => {
+        console.log('body', body);
+    });
 }
 
 export const TweetSearchField = () => <SearchField onSearchHandler={onSearchTwitter} placeholder="Search for a tweet" />;
