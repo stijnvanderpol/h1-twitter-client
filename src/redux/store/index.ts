e85d39d031d19dca9d2201173cc566e475098ed2
Stijn van der Pol <s.o.vanderpol@gmail.com>
2020-01-18 23:22:12 +0100

    enable redux devtools

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,4 +1,7 @@
 import { createStore } from 'redux';
 import { rootReducer } from '../reducers';
 
-export default createStore(rootReducer);
+export default createStore(rootReducer,
+    // @ts-ignore
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
