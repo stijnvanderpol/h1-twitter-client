a0d5e4c6373a23b341f954d6e1a1ff80059048cb
Stijn van der Pol <s.o.vanderpol@gmail.com>
2020-01-20 23:45:23 +0100

    add port number for deployment

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,11 +1,13 @@
 export const get = (endpoint: string, queryParams?: string) => {
     const apiHost = process.env.REACT_APP_API_HOST; 
+    const port = process.env.REACT_APP_API_PORT;
 
     if (!apiHost) {
         throw new Error('no api host defined!');
     }
+    
 
-    let uri = apiHost + '/api' + endpoint;
+    let uri = apiHost + `${port && ':'+port}` + '/api' + endpoint;
     
     if (queryParams) {
         uri += `?q=${queryParams}`;
